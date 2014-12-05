09e80f2b62ce5c0bbfeb328f9dae3fd1845e554e
Daniel Nguyen <dxungngh@gmail.com>
2014-12-05 22:17:51 +0700

    refactor MainActivity

--- BookStore/app/src/main/java/bookstore/daniel/com/bookstore/activity/MainActivity.java
+++ BookStore/app/src/main/java/bookstore/daniel/com/bookstore/activity/MainActivity.java
@@ -1,39 +1,28 @@
 package bookstore.daniel.com.bookstore.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import bookstore.daniel.com.bookstore.R;
 import bookstore.daniel.com.bookstore.fragment.NavigationDrawerFragment;
+import bookstore.daniel.com.bookstore.fragment.PlaceholderFragment;
 
 
 public class MainActivity extends ActionBarActivity
     implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private NavigationDrawerFragment mNavigationDrawerFragment;
-
     private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-            getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-            R.id.navigation_drawer,
-            (DrawerLayout) findViewById(R.id.drawer_layout));
+        setComponentViews();
+        setupComponentViews();
     }
 
     @Override
@@ -59,44 +48,14 @@ public class MainActivity extends ActionBarActivity
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+    private void setComponentViews() {
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+            getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
     }
 
+    private void setupComponentViews() {
+        mNavigationDrawerFragment.setUp(
+            R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
 }
